1c03c1133234118bf661a8f035121bcf4b50f59a
MattyC6969 <10145@bridgend.ac.uk>
2020-09-14 08:39:22 +0000

    my attempt

--- src/exercise.py
+++ src/exercise.py
@@ -1,7 +1,8 @@
 def main():
-  print("Once apon a time")
-  print("there was") 
-  print("a dinosaur")
+    print("Once upon a time")
+    print("there was")
+    print("a dinosaur")
+
 
 if __name__ == '__main__':
-  main()
+    main()
